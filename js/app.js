a554419a34e9c8f0472fd3a7d622d28215183b11
Bohdan Bordiuh <b.bordiuh@sana-commerce.com>
2022-10-03 10:38:47 +0300

    add contacts and footer

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 var common = {
 	init: function() {
 		common.main();
-		common.owl();
 	},
 	main: function(){
 
 		$('.menu-trigger').click(function(event){
 			event.preventDefault();
 			$('.header').toggleClass('open');
-		})
+		});
 		
+		$(".field").on("keyup change", function(e) {
+			$(this).val() != '' ? $(this).addClass('val') : $(this).removeClass('val');
+		});
+
 		// var bLazy = new Blazy({});
 
 
-	},
-	owl: function(){
-		
 	},
 };
 
